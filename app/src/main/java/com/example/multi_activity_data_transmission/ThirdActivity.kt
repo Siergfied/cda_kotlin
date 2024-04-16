df32f5e1aa6f352827b378f958690a669dce6ef0
Axel <axel.damart@gmail.com>
2024-04-16 10:04:20 +0200

    add control in MainActivity

--- app/src/main/java/com/example/multi_activity_data_transmission/ThirdActivity.kt
+++ app/src/main/java/com/example/multi_activity_data_transmission/ThirdActivity.kt
@@ -1,28 +1,20 @@
 package com.example.multi_activity_data_transmission
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class ThirdActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_third)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        val button = findViewById<Button>(R.id.start_second_activity)
-        button.setOnClickListener{
-            val intent = Intent(this, SecondActivity::class.java)
-            startActivity(intent)
         }
     }
 }
